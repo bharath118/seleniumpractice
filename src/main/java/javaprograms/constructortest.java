9a98fc58822265e7bae3384df44457e603a0626c
Bharath <bharath.raja118@gmail.com>
2025-07-03 14:12:43 +0530

    added constructor

---
+++ src/main/java/javaprograms/constructortest.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class constructortest {
+	
+	public constructortest(int a, int b) {
+		System.out.println("Parameterized constructor called with values: " + a + " and " + b);
+	}
+
+	public static void main(String[] args) {
+		
+		constructortest ct = new constructortest(10,20);
+		
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
