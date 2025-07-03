7f16d548e5d33ad01cd6753a1735946ef0e0217e
Bharath <bharath.raja118@gmail.com>
2025-07-03 11:20:46 +0530

    added subtraction

---
+++ src/main/java/javaprograms/subtraction.java
@@ -0,0 +1,21 @@
+package javaprograms;
+
+
+public class subtraction {
+	
+	private static int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		a = 10;
+		b = 5;
+		int result = subtract(a, b);
+		System.out.println("Subtraction of " + a + " and " + b + " is: " + result);
+
+	}
+
+}
